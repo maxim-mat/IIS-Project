5af4bd6517e7a85eb9d6885997efc483c70169d7
maxim-mat <maximm778@gmail.com>
2021-07-29 16:28:39 +0300

    Update Robot.py

--- Robot.py
+++ Robot.py
@@ -1,4 +1,4 @@
-import mdptoolbox
+import mdptoolbox as mdptb
 import numpy as np
 
 
@@ -6,19 +6,35 @@ def create_dict_to_index(container):
     return {k: i for i, k in enumerate(container)}
 
 
-def create_mdp_T_R(states, robot_actions,human_actions, final_state):
-    T = np.ones((len(states),len(robot_actions),len(states)), dtype=float) / len((states))
-    R = np.zeros((len(states),len(robot_actions)))
+def create_mdp_T_R(states, robot_actions, human_actions, final_state):
+    T = np.abs(np.random.normal(size=(len(robot_actions), len(states), len(states))))
+    for i, ac in enumerate(T):
+        T[i] /= ac.sum(axis=0)
+    R = np.ones((len(states), len(robot_actions)), dtype=float)
 
     # need to init R of final state to 100
     # need to represent human actions in T - need to calculate result as the result of both actions
-    return T,R
+    return T, R
+
+
+def cross_train(T, R, Simulation=None):
+    """
+    0.9 discount used in paper
+    :param T:
+    :param R:
+    :param Simulation:
+    :return:
+    """
+    solver = mdptb.mdp.ValueIteration(T, R, 0.9)
+    solver.run()
+    pi = solver.policy
+    print(pi)
 
 
 def print_T(T, state_dict, actions_dict):
     print("state action")
-    for s,v in state_dict.items():
-        for a,va in actions_dict.items():
+    for s, v in state_dict.items():
+        for a, va in actions_dict.items():
             print(s + ":    " + a +"    " + str(T[v][va]))
     return
 
@@ -43,10 +59,11 @@ def update_R(R, simulation_sequance):
 
 
 if __name__ == '__main__':
-    T,R = create_mdp_T_R(["a","b","c"], ["d", "w"], ["a"], 3)
-    #print(T)
-    print(R)
-    state_dict = create_dict_to_index(["a","b","c"])
-    #print(state_dict)
-    robot_actions_dict = create_dict_to_index( ["d", "w"])
-    print_T(T, state_dict, robot_actions_dict)
+    T, R = create_mdp_T_R(["a","b","c"], ["d", "w"], ["a"], 3)
+    cross_train(T, R)
+    # #print(T)
+    # print(R)
+    # state_dict = create_dict_to_index(["a","b","c"])
+    # #print(state_dict)
+    # robot_actions_dict = create_dict_to_index( ["d", "w"])
+    # print_T(T, state_dict, robot_actions_dict)
